f11bcd27755d7083a57e80f844196a0534967178
Santiago Correa Muñoz <santiago.correa@qualifyze.com>
2023-11-21 09:33:38 +0100

    telescope changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,11 +4,12 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local builtin = require('telescope.builtin')
+
 		vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>e', builtin.buffers, {})
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>s', function()
 			builtin.grep_string({search = vim.fn.input("Grep > ")})
-		end)
+		end, {})
 	end
 }
