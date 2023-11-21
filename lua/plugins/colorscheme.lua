3b61a872bdeb9f838e251fcd09cf1c6796bca158
Santiago Correa Muñoz <santiago.correa@qualifyze.com>
2023-11-21 09:39:58 +0100

    remove transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,5 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme "catppuccin"
-
-		vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-		vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 	end
 }
