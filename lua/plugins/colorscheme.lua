d3fac55a10fee7d36666dff0242e5c231603b63f
Santiago Correa Muñoz <santiago.correa@qualifyze.com>
2023-11-21 09:24:09 +0100

    added transparency background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,5 +5,8 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme "catppuccin"
+
+		vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+		vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 	end
 }
