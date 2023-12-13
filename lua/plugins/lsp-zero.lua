bcbe69e749e114e20d5718272e1c2678b222a469
Santiago Correa Muñoz <santiago.correa@qualifyze.com>
2023-12-13 12:54:45 +0100

    added eslint

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -89,7 +89,7 @@ return {
 			end)
 
 			require('mason-lspconfig').setup({
-				ensure_installed = { 'tsserver' },
+				ensure_installed = { 'tsserver', 'eslint' },
 				handlers = {
 					lsp_zero.default_setup,
 					lua_ls = function()
--- lua/santi/settings.lua
+++ lua/santi/settings.lua
@@ -1,9 +1,9 @@
 -- vim.o.hidden=true --Required to keep multiple buffers open
-vim.wo.wrap=false --Display long lines as just one line
+-- vim.wo.wrap=false --Display long lines as just one line
 vim.o.splitbelow=true --Horizontal splits will automatically be below
 vim.o.splitright=true --Vertical splits will automatically be to the right
 vim.wo.number = true -- show line number
 vim.wo.relativenumber = true -- show relative line number
 vim.wo.cursorline=true --Enable highlighting of the current line
- vim.o.showmode=false --We don't need to see things like -- INSERT -- anymore
--- vim.o.clipboard="unnamedplus" --Copy paste between vim and everything else
+vim.o.showmode=false --We don't need to see things like -- INSERT -- anymore
+vim.o.clipboard="unnamedplus" --Copy paste between vim and everything else
