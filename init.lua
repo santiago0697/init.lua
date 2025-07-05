2a52a30c8225d9da8b055c5fc0b33f9b1673bfd8
Santiago Correa <scorrea153@gmail.com>
2025-07-05 19:41:14 +0200

    feat: added lsp support

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require("core.lsp")
+
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
---
+++ lua/core/lsp.lua
@@ -0,0 +1,25 @@
+local icons = require('config.icons')
+
+vim.diagnostic.config({
+  -- virtual_text = true,
+  virtual_lines = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
+      [vim.diagnostic.severity.WARN] = icons.diagnostics.warn,
+      [vim.diagnostic.severity.INFO] = icons.diagnostics.info,
+      [vim.diagnostic.severity.HINT] = icons.diagnostics.hint,
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
+    },
+  },
+})
